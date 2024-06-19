41135c821f8063d305a52330843998b7722c39b9
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-06-19 19:57:13 +0530

    chore: WIP non working logic overriding

--- src/website/shared/admin.py
+++ src/website/shared/admin.py
@@ -1,1 +1,51 @@
 # Register your models here.
+
+
+from typing import Any
+
+from django.apps import apps
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+
+
+class CustomAdminSite(AdminSite):
+    def has_permission(self, request: Any) -> Any:  # type: ignore
+        if request.user.is_authenticated:
+            if request.user.is_staff:
+                return True
+            return super().has_permission(request)  # type: ignore
+        return False
+
+
+class CustomModelAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request: Any) -> Any:  # type: ignore
+        print("has_view_permission")
+        return True
+
+    def has_add_permission(self, request: Any) -> Any:
+        return True
+
+    def has_change_permission(self, request: Any) -> Any:
+        return True
+
+    def has_delete_permission(self, request: Any) -> Any:
+        return True
+
+
+custom_admin_site = CustomAdminSite()
+
+models = apps.get_models()
+for model in models:
+    custom_admin_site.register(model, CustomModelAdmin)
+
+
+# Unregister the provided model admin
+# admin.site.unregister(DefaultUser)
+# admin.site.register(User)
+
+# @admin.register(User)
+# class CustomUserAdmin(DefaultUserAdmin):
+#    def has_view_permission(self, request: Any, obj=None): return True
+#    def has_create_permission(self, request: Any, obj=None): return True
+#    def has_change_permission(self, request: Any, obj=None): return True
+#    def has_delete_permission(self, request: Any, obj=None): return True
--- src/website/shared/models/accounts.py
+++ src/website/shared/models/accounts.py
@@ -1,9 +1,61 @@
+from typing import Any
+
 from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     @property
+    def is_staff(self) -> bool:
+        return True
+
+    @property
     def github_account(self) -> SocialAccount | None:
         # As we only have one social account per user (GitHub), we use the first one.
         return self.socialaccount_set.first()  # type: ignore
+
+    def has_view_permission(self, request: Any, obj: Any) -> Any:
+        print("has_view_permission")
+        return True
+
+    def has_create_permission(self, request: Any, obj: Any) -> Any:  # type: ignore
+        return True
+
+    def has_change_permission(self, request: Any, obj: Any) -> Any:  # type: ignore
+        return True
+
+    def has_delete_permission(self, request: Any, obj: Any) -> Any:  # type: ignore
+        return True
+
+    def has_perm(self, perm: Any, obj: Any) -> Any:  # type: ignore
+        print("here?", perm, self.username)
+        # Check if the user has the specified permission
+        # You can customize the logic based on your needs
+        # if self.is_staff:
+        return True
+        return super().has_perm(perm, obj)
+
+
+class UserAdmin(User):
+    @property
+    def is_staff(self) -> bool:
+        print("here2")
+        return True
+
+
+"""
+
+    all_permissions = Permission.objects.all()
+
+    # Assign all permissions to a staff user
+    self.user_permissions.set(all_permissions)
+
+# Permissions
+from django.contrib.auth.models import Permission
+
+# Get all available permissions
+all_permissions = Permission.objects.all()
+
+# Assign all permissions to a staff user
+staff_user.user_permissions.set(all_permissions)
+"""
