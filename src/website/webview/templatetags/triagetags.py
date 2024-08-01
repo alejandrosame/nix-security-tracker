41ba03eb2d0e1466ae90bf7d806454b7702862ff
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-08-01 13:35:08 +0100

    fix: count of CPEs after cleaning Nones

--- src/website/webview/templatetags/triagetags.py
+++ src/website/webview/templatetags/triagetags.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterable
 from typing import Any
 
 from django import template
@@ -7,8 +6,8 @@ register = template.Library()
 
 
 @register.filter
-def clean_nones(input: Iterable[Any]) -> Iterable[Any]:
-    return filter(lambda e: e is not None, input)
+def clean_nones(input: list[Any]) -> list[Any]:
+    return [e for e in input if e is not None]
 
 
 @register.filter
