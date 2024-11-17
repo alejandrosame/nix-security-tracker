38e7319482056ac623487faf5614723526f3b166
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-11-17 02:16:01 -0300

    feat: collect and show suggestions activity log

--- src/website/webview/views.py
+++ src/website/webview/views.py
@@ -1,6 +1,7 @@
 import re
 import typing
 from collections.abc import Callable
+from functools import lru_cache
 from itertools import chain
 from typing import Any, cast
 
@@ -12,6 +13,7 @@ if typing.TYPE_CHECKING:
     from django.db.models.query import ValuesQuerySet
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.contrib.postgres.search import (
     SearchQuery,
@@ -37,7 +39,9 @@ from django.views.generic import DetailView, ListView, TemplateView
 from shared.models import (
     AffectedProduct,
     Container,
+    CVEDerivationClusterProposalStatusEvent,  # type: ignore
     CveRecord,
+    DerivationClusterProposalLinkEvent,  # type: ignore
     IssueStatus,
     NixChannel,
     NixDerivation,
@@ -482,6 +486,65 @@ class NixderivationPerChannelView(ListView):
         return context
 
 
+@lru_cache
+def get_username(user_id: int) -> str:
+    if user_id is None:
+        # TODO: Delete this case once the operations are guarded by
+        # auth, or consolidate to signal an inconsistent state to be checked
+        # by admins.
+        # NOTE(alejandrosame): These operations shouldn't be anonymous,
+        # but leaving this case explicitly tagged as anonymous user to avoid
+        # confusion with DELETED users.
+        return "ANONYMOUS"
+
+    if not User.objects.filter(id=user_id).exists():
+        # If user doesn't exist, we assumed it was deleted from the database
+        # at their request.
+        return "REDACTED"
+
+    return User.objects.get(id=user_id).username
+
+
+# TODO: add proper type for pgh_context
+@lru_cache
+def get_user_from_context(context: Any) -> str:
+    if context is None:
+        # An empty context means that the action took place
+        # from a management command by a superadmin.
+        return "ADMIN"
+
+    user_id = context.metadata.get("user")
+    return get_username(user_id)
+
+
+def get_suggestion_activity_log(suggestion: CVEDerivationClusterProposal) -> list[str]:
+    log = []
+
+    # Suggestion creation, deletion and status updates
+    for event in CVEDerivationClusterProposalStatusEvent.objects.prefetch_related(
+        "pgh_context",
+    ).filter(pgh_obj_id=suggestion.pk):
+        user = get_user_from_context(event.pgh_context)
+        operation = event.pgh_label
+        timestamp = event.pgh_created_at
+        log.append(f"@{user} did {operation} status to {event.status} at {timestamp}")
+
+    # Suggestion creation, deletion and package updates (additions and removals)
+    count = 0  # TODO: get rid of the count once the abysmal performance is fixed
+    for event in DerivationClusterProposalLinkEvent.objects.prefetch_related(
+        "pgh_context", "derivation"
+    ).filter(proposal_id=suggestion.pk):
+        if count == 10:
+            break
+        user = get_user_from_context(event.pgh_context)
+        operation = event.pgh_label
+        timestamp = event.pgh_created_at
+        log.append(f"@{user} did {operation} package {event.derivation} at {timestamp}")
+        count += 1
+
+    return log
+
+
 class SuggestionListView(ListView):
     template_name = "suggestion_list.html"
     model = CVEDerivationClusterProposal
@@ -494,6 +557,8 @@ class SuggestionListView(ListView):
 
         for obj in context["object_list"]:
             obj.packages = channel_structure(obj.derivations.all())
+            obj.activity_log = get_suggestion_activity_log(obj)
+
         context["adjusted_elided_page_range"] = context[
             "paginator"
         ].get_elided_page_range(context["page_obj"].number)
@@ -547,6 +612,8 @@ class DismissedListView(ListView):
 
         for obj in context["object_list"]:
             obj.packages = channel_structure(obj.derivations.all())
+            obj.activity_log = get_suggestion_activity_log(obj)
+
         context["adjusted_elided_page_range"] = context[
             "paginator"
         ].get_elided_page_range(context["page_obj"].number)
@@ -595,6 +662,8 @@ class DraftListView(ListView):
 
         for obj in context["object_list"]:
             obj.packages = channel_structure(obj.derivations.all())
+            obj.activity_log = get_suggestion_activity_log(obj)
+
         context["adjusted_elided_page_range"] = context[
             "paginator"
         ].get_elided_page_range(context["page_obj"].number)
